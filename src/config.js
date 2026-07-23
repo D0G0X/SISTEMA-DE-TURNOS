4fc394e3ae3bab6f5695243d79f9b691a78560c8
D0G0X <derlis2361@gmail.com>
2026-07-22 22:08:51 -0500

    cambios chiqui

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 (function () {
   var localApi = 'http://127.0.0.1:3001';
-  var renderApi = 'https://sistema-turnos-api.onrender.com';
+  var renderApi = 'https://sistema-de-turnos-ixgx.onrender.com';
   var savedApi = localStorage.getItem('TURNOS_API_URL');
   var isLocal = ['localhost', '127.0.0.1', ''].includes(window.location.hostname);
 
